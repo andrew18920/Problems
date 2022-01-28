355fdbfa7ad5182cf1792a8f408559e307b52a12
Andrew Connor <andrewjamesconnor@gmail.com>
2022-01-28 15:08:28 +0000

    added pair_diff

--- arr_pairs.py
+++ arr_pairs.py
@@ -4,12 +4,25 @@ def pair_sum(a: list, k: int) -> list:
     for item in a:
         hashmap[item] = k - item
         if k - item in hashmap:
-            pairs.add((item, k-item))
+            pairs.add((item, k - item))
+    return pairs
+
+
+def pair_diff(a: list, diff: int) -> list:
+    hashmap = {}
+    pairs = set()
+    for item in a:
+        hashmap[item] = item - diff
+        if item - diff in hashmap:
+            pairs.add((item, item - diff))
+        if item + diff in hashmap:
+            pairs.add((item, item + diff))
     return pairs
 
 
 def main():
-    print(pair_sum([1, 2, 3, 4, 5, 0], 5))
+    print(f"{pair_sum([1, 2, 3, 4, 5, 0], 5) = }")
+    print(f"{pair_diff([-1, 1, 3, 4, 5, 6, 4, 3, 10, 6], 3) = }")
 
 
 if __name__ == "__main__":
