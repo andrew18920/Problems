8d03030d83536e476da90dc4f412e338189ea14d
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-17 07:48:21 +0000

    optimise solution

--- max_area.py
+++ max_area.py
@@ -8,18 +8,21 @@ Return the maximum amount of water a container can store.
 
 
 def max_area(height: list[int]) -> int:
-    # brute force
-    max_area = 0
-    for i, a in enumerate(height):
-        for j, b in enumerate(height[i + 1 :]):
-            area = min(a, b) * (j + 1)
-            if area > max_area:
-                max_area = area
-    return max_area
+    # start v end, if start < end: increment start. if end < start: decrement end
+    i, j = 0, len(height) - 1
+    level = 0
+    while i < j:
+        level = max(level, (j - i) * min(height[i], height[j]))
+        if height[i] < height[j]:
+            i += 1
+        else:
+            j -= 1
+    return level
 
 
 def main():
     print(f"{max_area([1,8,6,2,5,4,8,3,7]) = }")
+    print(f"{max_area([1,1]) = }")
 
 
 if __name__ == "__main__":
