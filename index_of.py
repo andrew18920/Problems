2258b6a3749ca2aa3906e5743ca6128304c40607
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-17 00:05:46 +0000

    use next

--- index_of.py
+++ index_of.py
@@ -3,10 +3,14 @@ def index_of(haystack: str, needle: str) -> int:
         return 0
     if len(needle) > len(haystack):
         return -1
-    for i in range(0, len(haystack) + len(needle) + 1):
-        if haystack[i : i + len(needle)] == needle:
-            return i
-    return -1
+    return next(
+        (
+            i
+            for i in range(len(haystack) + len(needle) + 1)
+            if haystack[i : i + len(needle)] == needle
+        ),
+        -1,
+    )
 
 
 def main():
