8b2622e22fa39721d61b08dafcd7d0e429f59a95
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-18 15:35:27 +0000

    add pair diff

--- JavaScript/arr_pairs.js
+++ JavaScript/arr_pairs.js
@@ -10,4 +10,20 @@ var pair_sum = function (a, k) {
     return pairs;
 };
 
+var pair_diff = function (a, diff) {
+    item_diff = {}
+    pairs = new Set();
+    a.forEach(item => {
+        item_diff[item] = item - diff;
+        if (item - diff in item_diff) {
+            pairs.add(`${item}, ${item - diff}`)
+        };
+        if (item + diff in item_diff) {
+            pairs.add(`${item}, ${item + diff}`)
+        };
+    });
+    return pairs;
+}
+
 console.log(pair_sum([1, 2, 3, 4, 5, 0], 5))
+console.log(pair_diff([-1, 1, 3, 4, 5, 6, 4, 3, 10, 6], 3))
