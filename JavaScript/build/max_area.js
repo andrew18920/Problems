6eb864c6f69df8cb183a72d7d3786b83b559e9b5
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-21 23:50:02 +0000

    add max area

---
+++ JavaScript/build/max_area.js
@@ -0,0 +1,30 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+*/
+const maxArea = (height) => {
+    let [i, j] = [0, height.length - 1];
+    let level = 0;
+    while (i < j) {
+        level = Math.max(level, (j - i) * Math.min(height[i], height[j]));
+        if (height[i] < height[j]) {
+            i += 1;
+        }
+        else {
+            j -= 1;
+        }
+    }
+    return level;
+};
+const run = [
+    [1, 8, 6, 2, 5, 4, 8, 3, 7],
+    [1, 1],
+    [3, 4, 56, 1],
+    [0, 1, 2, 0, 4, 1],
+];
+run.forEach((s) => {
+    console.log(`maxArea([${s}]) = ${maxArea(s)}`);
+});
