ce600c31c3454e9b69408bb6cd46b5f8c755ae07
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-22 14:18:19 +0000

    solve check brackets

---
+++ JavaScript/check_brackets.ts
@@ -0,0 +1,26 @@
+const checkBrackets = (s: string): boolean => {
+  const brackets = new Map([
+    [']', '['],
+    ['}', '{'],
+    [')', '('],
+    ['>', '<'],
+  ]);
+  const found = [];
+  for (let i = 0; i < s.length; i += 1) {
+    const ch = s[i];
+    if (Array.from(brackets.values()).includes(ch)) {
+      found.push(ch);
+    } else if (brackets.has(ch)) {
+      if (found.length === 0 || brackets.get(ch) !== found.pop()) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return found.length === 0;
+};
+
+['<()>', '[', '()[]{}<>', '(]', '([)]'].forEach((s) => {
+  console.log(`checkBrackets('${s}') = ${checkBrackets(s)}`);
+});
