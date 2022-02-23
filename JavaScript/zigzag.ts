41e248158373f7bc1141fa60e08a23a14220950d
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-23 12:38:38 +0000

    start zigzag

---
+++ JavaScript/zigzag.ts
@@ -0,0 +1,16 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag
+pattern on a given number of rows like this:
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+*/
+
+const convert = (s: string, numRows: number): string => {
+  const i = 0;
+  const step = numRows === 0 ? 0 : -1;
+  return '';
+};
