625729e788e164593362cdfebc21198ceb4b4609
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-28 23:03:03 +0000

    use any

--- Python/ransom.py
+++ Python/ransom.py
@@ -5,15 +5,15 @@ from collections import Counter
 def ransom_possible(ransom: str, magazine: str) -> bool:
     ransom_counter = Counter(ransom)
     magazine_counter = Counter(magazine)
-    for ch, freq in ransom_counter.items():
-        if ch not in magazine_counter or magazine_counter[ch] < freq:
-            return False
-    return True
+    return not any(
+        ch not in magazine_counter or magazine_counter[ch] < freq
+        for ch, freq in ransom_counter.items()
+    )
 
 
 def main():
     print(ransom_possible("hexllo", "elloh pycharm"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
