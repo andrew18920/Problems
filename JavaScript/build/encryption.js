91c1ba3a3d84dd006b8e8fce2680a5d8270ece0a
Andrew Connor <andrewjamesconnor@gmail.com>
2022-02-28 22:38:49 +0000

    add encyption

---
+++ JavaScript/build/encryption.js
@@ -0,0 +1,17 @@
+const encryption = (s) => {
+    const s1 = s.replace(/\s+/g, '');
+    const colLength = Math.ceil(Math.sqrt(s1.length));
+    const columns = new Array(colLength).fill('');
+    for (let i = 0; i < s1.length; i += 1) {
+        columns[i % colLength] += s1[i];
+    }
+    return columns.join(' ');
+};
+[
+    'have a nice day',
+    'feed the dog      ',
+    'chill out',
+    'if man was meant to stay on the ground god would have given us roots',
+].forEach((s) => {
+    console.log(`encryption('${s}') = '${encryption(s)}'`);
+});
